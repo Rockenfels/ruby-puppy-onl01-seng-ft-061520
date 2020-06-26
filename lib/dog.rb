9bb8e3ae9f6101c64490e774a10a7ed520c48496
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-26 19:34:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Add your code here
+class Dog
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @@all << self
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def self.save
+    @@all << self
+  end
+  
+end
